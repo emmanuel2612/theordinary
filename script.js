f99784eb8275a9299e40b49d7ec2a4a4db5070dd
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-06-25 18:14:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 window.onload = () => {
 
-    window.scrollTo(0,1);
+   
+
+
+    /* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+openFullscreen();
 
 
 
